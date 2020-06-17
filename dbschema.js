8f8e9d490b92c2169750962613f1c8872fd2280e
Pavan Singhal <Pavan.Singhal@smartcontrols.in>
2020-06-17 17:20:31 +0530

    db Schema file

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams : [
+        {
+            userHandle : 'user',
+            body : 'this is the scream body',
+            createdAt : '2020-06-17T10:13:45.184Z',
+            likeCount : 5,
+            commentCount: 2
+        }
+    ]
+}
